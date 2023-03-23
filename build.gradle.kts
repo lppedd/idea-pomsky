d4575b702fd7b432845f1996ce12f86cc8106daf
Edoardo Luppi <lp.edoardo@gmail.com>
2023-03-23 18:57:09 +0100

    build: update build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,14 @@
+import org.jetbrains.grammarkit.tasks.GenerateLexerTask
+import org.jetbrains.grammarkit.tasks.GenerateParserTask
+
 plugins {
   id("java")
-  id("org.jetbrains.intellij") version "1.10.1"
+  id("org.jetbrains.intellij") version "1.13.2"
+  id("org.jetbrains.grammarkit") version "2022.3.1"
 }
 
 group = "com.github.lppedd"
-version = "0.0.2"
+version = "0.0.3"
 
 repositories {
   mavenCentral()
@@ -23,11 +27,15 @@ java {
   targetCompatibility = JavaVersion.VERSION_17
 }
 
-// See https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
   version.set("2022.3")
   type.set("IC")
   plugins.set(listOf("java" /* Only for code documentation */))
+}
+
+grammarKit {
+  jflexRelease.set("1.7.0-1")
+  grammarKitRelease.set("2022.3.1")
 }
 
 dependencies {
@@ -35,8 +43,40 @@ dependencies {
 }
 
 tasks {
+  wrapper {
+    distributionType = Wrapper.DistributionType.ALL
+  }
+
+  val generateLexer = task<GenerateLexerTask>("generatePomskyLexer") {
+    sourceFile.set(file("src/main/java/com/github/lppedd/idea/pomsky/lang/lexer/pomsky.flex"))
+    targetDir.set("src/main/gen/com/github/lppedd/idea/pomsky/lang/lexer")
+    targetClass.set("PomskyFlexLexer")
+    purgeOldFiles.set(true)
+  }
+
+  val generateParser = task<GenerateParserTask>("generatePomskyParser") {
+    dependsOn(generateLexer)
+
+    sourceFile.set(file("src/main/java/com/github/lppedd/idea/pomsky/lang/parser/pomsky.bnf"))
+    targetRoot.set("src/main/gen")
+    pathToParser.set("com/github/lppedd/idea/pomsky/lang/parser/PomskyGeneratedParser.java")
+    pathToPsiRoot.set("psi")
+    purgeOldFiles.set(true)
+  }
+
+  compileJava {
+    dependsOn(generateParser)
+  }
+
   patchPluginXml {
     sinceBuild.set("223")
-    untilBuild.set("231.*")
+    untilBuild.set("233.*")
+  }
+
+  runPluginVerifier {
+    ideVersions.set(listOf(
+        "IC-2022.3",
+        "IC-2023.1",
+    ))
   }
 }
